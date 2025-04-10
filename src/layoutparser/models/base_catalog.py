0e449fe36207c3bf8ec96234dfff17557425f9c8
Verena Jasmin Hallitschke <95714954+verena-hallitschke@users.noreply.github.com>
2025-04-10 18:49:17 +0200

    Add missing imports

--- src/layoutparser/models/base_catalog.py
+++ src/layoutparser/models/base_catalog.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from typing import Any, Optional
+
 from iopath.common.file_io import HTTPURLHandler
 from iopath.common.file_io import PathManager as PathManagerBase
 
